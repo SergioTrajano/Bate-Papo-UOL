0e999023bfaed485f5f52d0a88f9ca0332402aec
Sergio Trajano <sergio.trajanosilva2001@gmail.com>
2022-04-15 16:06:15 -0300

    Funcionalidade de enviar + layout do bonus 1

--- scripts.js
+++ scripts.js
@@ -1,3 +1,44 @@
+function seleciona(elemento, tipo) {
+    const apagar = document.querySelector(`.${tipo} .selecionado`);
+    const marca = elemento.querySelector(".check");
+    if (apagar !== null) {
+        apagar.querySelector(".check").classList.add("escondido");
+        apagar.classList.remove("selecionado");
+    }
+    elemento.classList.add("selecionado");
+    elemento.querySelector(".check").classList.remove("escondido");
+}
+
+function displayLateral(acao) {
+    const barraLateral = document.querySelector(".menu-lateral");
+    if (acao == 'esconder') {
+        barraLateral.classList.add("escondido");
+    }
+    if (acao == 'mostrar') {
+        barraLateral.classList.remove("escondido");
+    }
+}
+
+function recarregar(erro) {
+    alert(erro.response.status);
+    alert("Você perdeu a conexão com o servidor");
+    location.reload();
+}
+
+function enviaMensagem() {
+    const mesage = document.querySelector("input").value;
+    const msg = {
+        from: usuario.name,
+        to: "Todos",
+        text: mesage,
+        type: "message"
+    };
+    const prom = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", msg);
+    prom.then(atualizaMensagens);
+    prom.catch(recarregar);
+    document.querySelector("input").value = "";
+    document.querySelector("input").focus();
+}
 
 function permitido(resposta, i) {
     if (resposta.data[i].type === 'private_message' && resposta.data[i].to === usuario) {
@@ -11,30 +52,31 @@ function permitido(resposta, i) {
 
 function tratarSucess(resposta) {
     const numMesages = resposta.data.length;
-    const mesages = document.querySelector("ul");
+    const mesages = document.querySelector(".chat ul");
+    mesages.innerHTML = "";
     for (let i = 0; i < numMesages; i++) {
         if (permitido(resposta, i)) {
             mesages.innerHTML += `
-            <li class='${resposta.data[i].type}'><p><span> (${resposta.data[i].time})</span> <strong>  ${resposta.data[i].from} </strong> para <strong> ${resposta.data[i].to} </strong> ${resposta.data[i].text} </p></li>
+            <li class='${resposta.data[i].type}'><p><span> (${resposta.data[i].time})</span>&nbsp<strong>${resposta.data[i].from}</strong>&nbsppara&nbsp<strong>${resposta.data[i].to}</strong>:&nbsp${resposta.data[i].text} </p></li>
             `
         }
     }
-    const ultimaMensagem = document.querySelector(`li:nth-child(${document.querySelectorAll("li").length})`)
+    const ultimaMensagem = document.querySelector(`li:last-child`);
     ultimaMensagem.scrollIntoView(false);
 }
 
 function atualizaMensagens() {
-    const msgs = document.querySelector("ul");
-    msgs.innerHTML = "";
     const promisse = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
     promisse.then(tratarSucess);
 }
 
 function manterConexao() {
-    axios.post('https://mock-api.driven.com.br/api/v6/uol/status', usuario)
-};
+    const conexao = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', usuario);
+    conexao.catch(recarregar);
+}
 
 function tratarSucesso() {
+    atualizaMensagens();
     setInterval(manterConexao, 4000);
     setInterval(atualizaMensagens, 3000);
 }
